a87bcc9b8b0a78a0415e0f204b7ff4c7493223e4
CesarAVegaF <cesar.vega-f@mail.escuelaing.edu.co>
2017-07-15 14:13:57 -0500

    roverbot_comunication publica informacion de los obstaculos

--- roverbot_comunication/src/talkerLaser.cpp
+++ roverbot_comunication/src/talkerLaser.cpp
@@ -6,15 +6,22 @@
 #include "message_filters/subscriber.h"
 #include "laser_geometry/laser_geometry.h"
 #include "std_msgs/String.h"
+#include "std_msgs/MultiArrayLayout.h"
+#include "std_msgs/MultiArrayDimension.h"
+#include "std_msgs/Float32MultiArray.h"
+#include "roverbot/Obstacles.h"
+#include "roverbot/Point.h"
+
 #define PI 3.14159265
 using namespace std;
 unsigned int num_points = 100;
 
-float xBot = 30;
+float xBot = 6;
 float yBot = 0;
 float zBot, normaBot;
 float norma, prodIn, cosA;
 float xCloud, yCloud, zCloud;
+vector<double> vec;
 
 
 
@@ -37,7 +44,7 @@ public:
     laser_notifier_.registerCallback(
       boost::bind(&LaserScanToPointCloud::scanCallback, this, _1));
     laser_notifier_.setTolerance(ros::Duration(0.01));
-    scan_pub_ = n_.advertise<sensor_msgs::PointCloud>("cloud",1);
+    scan_pub_ = n_.advertise<roverbot::Obstacles>("roverbot/obstacles",1);
   }
 
   void scanCallback (const sensor_msgs::LaserScan::ConstPtr& scan_in)
@@ -63,20 +70,24 @@ public:
         return;
     }
 
-    printPoints(cloud);
+    scan_pub_.publish(printPoints(cloud));
 
-    //scan_pub_.publish(cloud);
+    ROS_INFO("I published something!");
   }
 
   /**
   * Funcion para el tratamiento de la informacion de la nube de puntos
   */
-  void printPoints(sensor_msgs::PointCloud cloud)
+  roverbot::Obstacles printPoints(sensor_msgs::PointCloud cloud)
   {
 
     num_points = cloud.points.size();  // número de puntos en la lectura del scan
 
+    roverbot::Obstacles msg;
+
     vector<float> normaBot;
+    normaBot.clear();
+
     zBot = cloud.points[0].z; // altura del robot o del laser
     // Caracteristicas del vector referencia al frente del robot
     normaBot.push_back(
@@ -92,6 +103,7 @@ public:
 
     //inicio del vector pasado posicion 0 de la nube de puntos
     vector<float> normaCloudPas; 
+    normaCloudPas.clear();
     normaCloudPas.push_back(
 	sqrt(
 		pow(cloud.points[0].x, 2.0)+
@@ -105,9 +117,15 @@ public:
 
     int obstaculos = 1; //número de obstaculos detectados
  
+    vector< roverbot::Point > data;
+    data.clear();
+
     //grupo de datos de un objeto
     vector< vector< float > > dataGroup;
+    dataGroup.clear();
+
     for(unsigned int i = 1; i < num_points; i++){
+
 	// Extrae las coordenadas x, y, z de los puntos de la nube
         xCloud = cloud.points[i].x;
         yCloud = cloud.points[i].y;
@@ -115,19 +133,21 @@ public:
 
 	//Vector de magnitud actual
 	vector<float> normaCloud;
+        normaCloud.clear();
         normaCloud.push_back(sqrt((xCloud*xCloud)+(yCloud*yCloud)+(zCloud*zCloud)));
 	normaCloud.push_back(xCloud);
 	normaCloud.push_back(yCloud);
 	normaCloud.push_back(zCloud);
 	
-	float rango = (normaCloud[0] * 7.5)/100.0; // Tolerancia de error
+	float rango = (normaCloud[0] * 10)/100.0; // Tolerancia de error
 	
 	dataGroup.push_back(normaCloudPas); // añade los datos al grupo
 
 	// si esta dentro de la tolerancia hace parte del grupo de datos del objeto
 	// Si no, hace parte del vertice del objeto, por lo tanto entra a la condicion y verifica el punto minimo
 	if (normaCloudPas[0] > normaCloud[0]+rango || normaCloudPas[0] < normaCloud[0]-rango){
-	    
+	    roverbot::Point point;
+
             //Distancia de comparación
             // ángulo y distancia del punto mas cerca
             vector<float> minDistance = normaBot; 
@@ -141,8 +161,10 @@ public:
 
 	    int sizeGroup = dataGroup.size();
 	    vector<float> floatTemp;
+            floatTemp.clear();
 	    // verifica todos los datos en el grupo encontrando el menor
 	    for(unsigned int j = 0; j < sizeGroup; j++){
+		
 		floatTemp = dataGroup[j];
 	        // cos(theta) = InnerPorduct(u, v) / |u|*|v| 
 	        norma = normaBot[0]*floatTemp[0]; // |u|*|v| 
@@ -178,17 +200,32 @@ public:
 		maxTheta *= -1;
 	    }
 	    // filtra casos donde esta en el borde del radar
-	    if(minDistance[0] < 29.8){
+	    if(minDistance[0] < xBot-((xBot*1)/100.0)){
 		printf("Obstaculo: %d\n", obstaculos++);
 		printf("Rango de vision sobre el obstaculo:\n"); 
 		printf("angulo --> %4.2f; distancia --> %4.4f m\n", minTheta, minNorma[0]);
 		printf("angulo --> %4.2f; distancia --> %4.4f m\n", maxTheta, maxNorma[0]);
 	        printf("Punto cercano: angulo --> %4.2f; distancia --> %4.4f m\n", theta, minDistance[0]);
+
+		point.min_theta = minTheta;
+		point.theta = theta;
+		point.max_theta = maxTheta;
+
+		point.min_norm = minNorma[0];
+		point.norm = minDistance[0];
+		point.max_norm = maxNorma[0];
+		
+		msg.obstacles.push_back(point);
 	    }
 	    dataGroup.clear(); // limpia el vector usado
 	}
 	normaCloudPas = normaCloud; // establece el punto actual como pasado.
     }
+
+    msg.size = msg.obstacles.size();
+    msg.label = "ros_topic: roverbot/Obstacles";
+
+    return msg;
 	
   }
 
@@ -197,11 +234,13 @@ public:
 int main(int argc, char** argv)
 {
   
-  ros::init(argc, argv, "my_scan_to_cloud");
+  ros::init(argc, argv, "my_scan_to_obstacles");
   ros::NodeHandle n;
+  while (ros::ok())
+  {
   LaserScanToPointCloud lstopc(n);
   
   ros::spin();
-  
+  }
   return 0;
 }
